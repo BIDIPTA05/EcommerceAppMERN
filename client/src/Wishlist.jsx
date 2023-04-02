72f40eee09c939c86a9d7106d871fc444f032b86
BIDIPTA05 <bidiptasaikia@gmail.com>
2023-04-02 23:41:17 +0530

    feat: add to cart from wishlist

--- client/src/Wishlist.jsx
+++ client/src/Wishlist.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
-import Wishlistitem from './Wishlist_items'
-import Footer from './Footer'
-import Navbar from "./Mainnavbar"
+import React, { useState } from "react";
+import Wishlistitem from "./Wishlist_items";
+import Footer from "./Footer";
+import Navbar from "./Mainnavbar";
+import { useParams } from "react-router-dom";
 
-
+//testing
 const Wishlist = () => {
+
+  const { id } = useParams();
+
+  const [show, setShow] = useState(true);
+  const [cart, setCart] = useState([]);
+
+  const handleClick = (product) => {
+    let isPresent = false;
+    cart.forEach((products) => {
+      if (product.id === products.id) isPresent = true;
+    });
+    if (isPresent) {
+      alert("Product already added to cart");
+      return;
+    }
+    setCart([...cart, product]);
+
+    console.log(cart);
+  };
+
   return (
     <>
-      <Navbar />
-      
-
-      <Wishlistitem />
+      <Navbar size={cart.length} setShow={setShow} />
+      <Wishlistitem label={id} handleClick={handleClick} />
       <Footer />
     </>
   );
-}
+};
 
-export default Wishlist
+export default Wishlist;
--- client/src/Wishlist_items.jsx
+++ client/src/Wishlist_items.jsx
@@ -1,42 +1,46 @@
 import { AiFillDelete } from "react-icons/ai";
+import Wishlist from "./Wishlist";
 
 const products = [
   {
     id: 1,
-
-    name: "Earthen Bottle",
-    href: "#",
-    price: "$48",
-    imageSrc: "https://www.linkpicture.com/q/ip14.jpg",
+    name: "iPhone 14",
+    price: 50000,
+    imageSrc: "https://www.linkpicture.com/q/ip13.jpeg",
     imageAlt:
       "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
   },
   {
     id: 2,
-    name: "Nomad Tumbler",
-    href: "#",
-    price: "$35",
-    imageSrc: "https://www.linkpicture.com/q/ip13.jpeg",
+    name: "iPhone 14 pro",
+    price: 45999,
+    imageSrc: "https://www.linkpicture.com/q/ip14.jpg",
     imageAlt:
       "Olive drab green insulated bottle with flared screw lid and flat top.",
   },
   {
     id: 3,
-    name: "Focus Paper Refill",
-    href: "#",
-    price: "$89",
-    imageSrc: "https://www.linkpicture.com/q/ip13.jpeg",
+    name: "iPhone 12",
+    price: 79999,
+    imageSrc: "https://www.linkpicture.com/q/ip12.jpg",
     imageAlt:
       "Person using a pen to cross a task off a productivity paper card.",
   },
   {
     id: 4,
-    name: "Machined Mechanical Pencil",
-    href: "#",
-    price: "$35",
-    imageSrc: "https://www.linkpicture.com/q/ip13.jpeg",
+    name: "iPhone 14 plus",
+    price: 85990,
+    imageSrc: "https://www.linkpicture.com/q/ipp14.jpg",
     imageAlt:
       "Hand holding black machined steel mechanical pencil with brass tip and top.",
+  },
+  {
+    id: 5,
+    name: "iPhone 14",
+    price: 37999,
+    imageSrc: "https://www.linkpicture.com/q/ip13.jpeg",
+    imageAlt:
+      "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
   },
 ];
 
@@ -46,7 +50,7 @@ export default function Example(props) {
       <div className="mx-auto max-w-2xl px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <h1 className="text-5xl font-bold text-black  mb-10">My Wishlist </h1>
 
-        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 ">
           {products.map((product) => (
             <a key={product.id} href={product.href} className="group">
               <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
@@ -96,10 +100,12 @@ export default function Example(props) {
               <br />
 
               <div className="flex space-x-10">
-                <button className="btn btn-accent ">Move to Cart</button>
+                <button className="btn btn-accent"  onClick={() => props.handleClick(product)}>
+                  Move to Cart
+                </button>
 
-                <a >
-                  <AiFillDelete className="text-2xl text-red-600 mt-2 " onmouseho />
+                <a>
+                  <AiFillDelete className="text-2xl text-red-600 mt-2 " />
                 </a>
               </div>
             </a>
