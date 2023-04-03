261bb38a373426650130f1bc7d7f7610692e2caa
BIDIPTA05 <bidiptasaikia@gmail.com>
2023-04-03 14:42:09 +0530

    feat: sort and filter done

--- client/src/Items.jsx
+++ client/src/Items.jsx
@@ -1,22 +1,25 @@
-import React from "react";
+import { React, useState } from "react";
 import { Products } from "./Products";
 import Sort from "./Sortnfilter";
 
 export default function Items(props) {
+  console.log({ Products });
+  const [products, setProducts] = useState(Products);
+
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <img
           src="https://www.linkpicture.com/q/highest-spender-offer-d_1.jpg"
           type="image"
-          className="w-full"
+          className=""
         />
-        <Sort />
+        <Sort products={products} setProducts={setProducts} />
         <h2 className="sr-only">Products</h2>
         <h1 className=" pb-10 text-4xl">{props.label} </h1>
 
         <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {Products.map((product) => (
+          {products.map((product) => (
             <a key={product.id} href={product.href} className="group">
               <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
                 <img
--- client/src/Sortnfilter.jsx
+++ client/src/Sortnfilter.jsx
@@ -1,35 +1,62 @@
-import React from "react";
+import { React, useState } from "react";
 
-const Sortnfilter = () => {
+const Sortnfilter = ({ products, setProducts }) => {
+const lowToHigh = () => {
+  const sortedProducts = [...products].sort((a, b) => a.price - b.price);
+  console.log(sortedProducts, "boom");
+  setProducts(sortedProducts);
+};
+
+  
+  const sort = (e) => {
+    const sortValue = e.target.value;
+    let sortedProducts = [...products];
+    console.log(sortValue);
+    if (sortValue === "low-to-high") {
+      setProducts(sortedProducts.sort((a, b) => a.price - b.price));
+    } else if (sortValue === "high-to-low") {
+      setProducts(sortedProducts.sort((a, b) => b.price - a.price));
+    }
+  }
+
+
+
   return (
     <>
-      <div className=" text-2xl mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 ">
-        <h2 className="pb-3 text-black">Sort and filter products: </h2>
-        <select className="select select-warning w-full max-w-xs  bg-stone-300 text-black border-none mr-8 mb-4">
-          <option disabled selected>
-            <h1>Sort By</h1>
-          </option>
-          <option value="#" disabled></option>
-          <option>Relevance</option>
-          <option value="#" disabled></option>
-          <option>Price-Low to High</option>
-          <option value="#" disabled></option>
-          <option>Price-High to Low</option>
-          <option value="#" disabled></option>
-        </select>
-        <select className="select select-warning w-full max-w-xs bg-stone-300 text-black border-none">
-          <option disabled selected>
-            <h1>Price Range</h1>
-          </option>
-          <option value="#" disabled></option>
-          <option>Rs.0-Rs.20000</option>
-          <option value="#" disabled></option>
-          <option value="">Rs.20000-Rs.50000</option>
-          <option value="#" disabled></option>
-          <option value="">Above Rs.50000</option>
-          <option value="#" disabled></option>
-        </select>
-      </div>
+      <form>
+        <div className=" text-2xl mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 ">
+          <h2 className="pb-3 text-black">Sort and filter products: </h2>
+
+          <select
+            className="select select-warning w-full max-w-xs  bg-stone-300 text-black border-none mr-8 mb-4"
+            name="sort"
+            onChange={sort}
+          >
+            <option disabled selected>
+              <h1>Sort By</h1>
+            </option>
+            <option disabled></option>
+            <option value="relevance">Relevance</option>
+            <option disabled></option>
+            <option value="low-to-high"> Price-Low to High</option>
+            <option disabled></option>
+            <option value="high-to-low">Price-High to Low</option>
+            <option disabled></option>
+          </select>
+          <select className="select select-warning w-full max-w-xs bg-stone-300 text-black border-none">
+            <option disabled selected>
+              <h1>Price Range</h1>
+            </option>
+            <option value="#" disabled></option>
+            <option>Rs.0-Rs.20000</option>
+            <option value="#" disabled></option>
+            <option value="">Rs.20000-Rs.50000</option>
+            <option value="#" disabled></option>
+            <option value="">Above Rs.50000</option>
+            <option value="#" disabled></option>
+          </select>
+        </div>
+      </form>
     </>
   );
 };
